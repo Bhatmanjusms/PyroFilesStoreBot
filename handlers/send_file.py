20c0add88fb71096b6df42ce855f68d44a5c9c57
Sahidmalik001 <106477157+Sahidmalik001@users.noreply.github.com>
2023-01-19 23:21:51 +0530

    Update send_file.py

--- handlers/send_file.py
+++ handlers/send_file.py
@@ -12,15 +12,15 @@ async def media_forward(bot: Client, user_id: int, file_id: int):
     try:
         if Config.FORWARD_AS_COPY is True:
             return await bot.copy_message(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                          message_id=file_id, protect_content=True)
+                                        protect_content=True,  message_id=file_id)
         elif Config.FORWARD_AS_COPY is False:
             return await bot.forward_messages(chat_id=user_id, from_chat_id=Config.DB_CHANNEL,
-                                              message_ids=file_id, protect_content=True)
+                                             protect_content=True, message_ids=file_id)
     except FloodWait as e:
         await asyncio.sleep(e.value)
         return media_forward(bot, user_id, file_id)
 
 
 async def send_media_and_reply(bot: Client, user_id: int, file_id: int):
-    sent_message = await media_forward(bot, user_id, file_id, protect_content=True) 
+    protect_content=True, sent_message = await media_forward(bot, user_id, file_id) 
     await asyncio.sleep(2)
